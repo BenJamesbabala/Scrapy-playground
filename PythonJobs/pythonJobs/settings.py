5379d568039bf69325bdf52e231130d9766ac8f7
BigBorg <770188954@qq.com>
2016-10-22 00:37:01 +0800

    Moving to python3

--- PythonJobs/pythonJobs/settings.py
+++ PythonJobs/pythonJobs/settings.py
@@ -86,7 +86,7 @@ ITEM_PIPELINES = {
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
+HTTPCACHE_ENABLED = False
 HTTPCACHE_EXPIRATION_SECS = 3600*60*24
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
